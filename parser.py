a50915a9f829fe5a686ed9acc5c4b270783c6fb9
Samuel Magnan <sam.samuel.magnan@gmail.com>
2018-04-04 21:16:14 +0200

    helpfulness score parsing

--- parser.py
+++ parser.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
+
 class Review(object):
     """docstring for Review"""
     def __init__(self):
         super().__init__()
 
     @staticmethod
-    def parse(xml_data):
+    def parse(xml_data: list):
         current_key = None
         data = {}
         for line in xml_data:
@@ -18,7 +19,19 @@ class Review(object):
                     data[current_key] = []
             else:
                 data[current_key].append(line)
+        pos_helpful, total_helpful = Review.helpful2score(data['helpful'])
+        data['pos_helpful'] = pos_helpful
+        data['total_helpful'] = total_helpful
         return data
+
+    @staticmethod
+    def helpful2score(helpful_list: list):
+        if len(helpful_list) > 1:
+            print("WARN: %s not containing just one helpfulness rating" % helpful_list)
+        try:
+            return int(helpful_list[0].split(' of ')[0]), int(helpful_list[0].split(' of ')[1])
+        except ValueError:  # d/D can't be parsed as ints
+            return 0.0, 0.0
 
 
 class AmazonReviewsParser(object):
@@ -42,6 +55,4 @@ class AmazonReviewsParser(object):
                 else:
                     review.append(line)
         return pd.DataFrame(reviews)
-
-
 

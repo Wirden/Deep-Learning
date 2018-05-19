52657d4eb1eba52823e6315b521c6a66d4385e73
Wirden <antoine.chevrot@gmail.com>
2018-05-19 22:44:12 +0200

    Changed the idea behind the first Test. Now tries to softmax a spacy generated vector. WIP_3

--- spacy_test.py
+++ spacy_test.py
@@ -12,7 +12,20 @@ import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
+
+def accuracy(predictions, labels):
+    correctly_predicted = np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+    accu = (100.0 * correctly_predicted) / predictions.shape[0]
+    return accu
+
 def main():
+    
+    # learning rate (alpha)
+    learning_rate = 0.05
+    # batch size
+    batch_size = 128
+    # number of epochs
+    num_steps = 5001
     
     nlp = spacy.load('en_core_web_lg')
     
@@ -29,8 +42,10 @@ def main():
     one_hot_labels = np.zeros((len(df),5))
     one_hot_labels[np.arange(len(df)), df.rating.values-1] = 1
     
-    X_train, X_test, y_train, y_test = 
-        train_test_split(df.review_vector,one_hot_labels, test_size=0.10)
+    X_train, X_test, y_train, y_test = train_test_split(
+            df.review_vector.values,one_hot_labels, test_size=0.10)
+    
+    print(type(X_test))
     
     graph = tf.Graph()
  
@@ -40,6 +55,7 @@ def main():
         
         W = tf.Variable(tf.zeros([300, 5]))
         b = tf.Variable(tf.zeros([5]))
+        test_dataset = tf.constant(X_test)
     
         logits = tf.matmul(x, W) + b
     
@@ -48,13 +64,34 @@ def main():
         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
                 labels=y_, logits=logits))
         
-        optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+        
+        train_prediction = tf.nn.softmax(logits)
+        test_prediction = tf.nn.softmax(tf.matmul(test_dataset, W) + b)
         
         with tf.Session(graph=graph) as session:
             tf.global_variables_initializer().run()
             print("Initialized")
             
-            
+            for step in range(num_steps):
+            # pick a randomized offset
+                offset = np.random.randint(0, y_train.shape[0] - batch_size - 1)
+                
+                # Generate a minibatch.
+                batch_data = X_train[offset:(offset + batch_size), :]
+                batch_labels = y_train[offset:(offset + batch_size), :]
+                
+                # Prepare the feed dict
+                feed_dict = {x : batch_data, y_ : batch_labels}
+                
+                # run one step of computation
+                _, l, predictions = session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+                
+                if (step % 500 == 0):
+                    print("Minibatch loss at step {0}: {1}".format(step, l))
+                    print("Minibatch accuracy: {:.1f}%".format(accuracy(predictions, batch_labels)))
+                
+                print("\nTest accuracy: {:.1f}%".format(accuracy(test_prediction.eval(), y_test)))
             
             
 
